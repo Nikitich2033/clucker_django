22e7729c9e4109a742f043f638fceb474fcbe376
Nikita Lyakhovoy <lyakhovoynikita@gmail.com>
2021-10-21 20:56:48 +0100

    Create sign up view

--- microblogs/tests/test_sign_up_form.py
+++ microblogs/tests/test_sign_up_form.py
@@ -4,6 +4,7 @@ from django import forms
 from django.test import TestCase
 from microblogs.forms import SignUpForm
 from microblogs.models import User
+from django.contrib.auth.hashers import check_password
 
 
 class SignUpFormTestCase(TestCase):
@@ -79,3 +80,17 @@ class SignUpFormTestCase(TestCase):
 
         form = SignUpForm(data = self.form_input)
         self.assertFalse(form.is_valid())
+
+    def test_form_must_save_correctly(self):
+        form = SignUpForm(data = self.form_input)
+        before_count = User.objects.count()
+        form.save()
+        after_count = User.objects.count()
+        self.assertEqual(after_count,before_count+1)
+        user = User.objects.get(username='@janedoe')
+        self.assertEqual(user.first_name,'Jane')
+        self.assertEqual(user.last_name,'Doe')
+        self.assertEqual(user.email,'janedoe@example.com')
+        self.assertEqual(user.bio,'My bio')
+        is_pass_Correct =  check_password('Password123',user.password)
+        self.assertTrue(is_pass_Correct)
