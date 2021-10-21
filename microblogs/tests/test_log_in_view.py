050fb0ad521919eb5b0b82dd4be5a6491ff57ecc
Nikita Lyakhovoy <lyakhovoynikita@gmail.com>
2021-10-21 22:03:43 +0100

    Process unsuccessful and successful log ins

--- microblogs/tests/test_log_in_view.py
+++ microblogs/tests/test_log_in_view.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from microblogs.forms import LogInForm
-
+from microblogs.models import User
 
 from django.contrib.auth.hashers import check_password
 
@@ -9,6 +9,14 @@ class LogInViewTestCase(TestCase):
 
     def setUp(self):
         self.url = reverse('log_in')
+        User.objects.create_user(
+            '@johndoe',
+            first_name = 'John',
+            last_name = 'Doe',
+            email = 'johndoe@example.org',
+            password = 'Password123',
+            bio = 'my name john'
+        )
 
     def test_sign_up_url(self):
         self.assertEqual(self.url,'/log_in/')
@@ -20,3 +28,24 @@ class LogInViewTestCase(TestCase):
         form = response.context['form']
         self.assertTrue(isinstance(form, LogInForm))
         self.assertFalse(form.is_bound)
+
+    def test_unsuccessful_log_in(self):
+        form_input = {'username':'@johndoe','password':'WrongPassword123'}
+        response = self.client.post(self.url,form_input)
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'log_in.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, LogInForm))
+        self.assertFalse(form.is_bound)
+        self.assertFalse(self._is_logged_in())
+
+    def test_successful_log_in(self):
+        form_input = {'username':'@johndoe','password':'Password123'}
+        response = self.client.post(self.url,form_input, follow = True)
+        self.assertTrue(self._is_logged_in())
+        response_url = reverse('feed')
+        self.assertRedirects(response, response_url,status_code=302,target_status_code = 200)
+        self.assertTemplateUsed(response,'feed.html')
+
+    def _is_logged_in(self):
+        return '_auth_user_id' in self.client.session.keys()
--- microblogs/views.py
+++ microblogs/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, redirect
 from .models import User
 from .forms import SignUpForm,LogInForm
+from django.contrib.auth import authenticate,login
 # Create your views here.
 
 def log_in(request):
+    if request.method == 'POST':
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('feed')
+
     form = LogInForm()
     return render(request,'log_in.html', {'form':form})
 
